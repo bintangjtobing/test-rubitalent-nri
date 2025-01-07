888d93325be23c4856ec839fc1b0a39bca3401d3
Bintang Tobing <hello@bintangtobing.com>
2025-01-07 16:02:42 +0700

    feat: analytics code track

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,22 @@
     <link rel="stylesheet" href="{{asset('fe/css/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="{{asset('fe/css/owl.theme.default.min.css')}}">
     <link rel="shortcut icon" href="{{asset('fe/images/favicon.ico')}}">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MV4280QWKQ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MV4280QWKQ');
+    </script>
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "pq3p757djz");
+    </script>
 </head>
 
 <body>
